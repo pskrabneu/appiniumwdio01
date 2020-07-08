55ce32a8d197ddfc6076585a691a54cb469377d7
Pavel Skrabneu <skrabneu@gmail.com>
2020-07-08 18:33:05 +0300

    Open "Home Page" test

--- test/specs/open-home-page.spec.js
+++ test/specs/open-home-page.spec.js
@@ -7,7 +7,6 @@ describe('Open Home Page:', function() {
   it('title should contains \'Course\' word', function() {
     HomePage.performSysAdminLogin();
     const pageTitle = browser.getTitle();
-    console.log('==> ' + pageTitle + ' <==');
     expect(pageTitle).toContain('Course');
     utilities.takeScreenshot('open_home_page');
   });
